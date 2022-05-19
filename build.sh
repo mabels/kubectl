d4efcd6875aaf31f51a45fd6804baa9676a24981
Meno Abels <meno.abels@adviser.com>
2022-05-19 19:52:14 +0200

    chore: fix another merge error

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-REPO=public.ecr.aws
+REPO=public.ecr.aws/mabels
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
 npm i
 eval $(node latest_versions.js "kubernetes/kubernetes")
@@ -35,30 +35,30 @@ do
   then
 	i=linux/arm
   fi 
-  docker buildx build -t ghcr.io/mabels/kubectl:$version-$(basename $i)-ubuntu-jammy --platform $i -f ./Dockerfile.ubuntu ./_output/local/bin/$i 
-  docker push ghcr.io/mabels/kubectl:$version-$(basename $i)-ubuntu-jammy
-  docker buildx build -t ghcr.io/mabels/kubectl:$version-$(basename $i)-debian-buster --platform $i -f ./Dockerfile.debian ./_output/local/bin/$i 
-  docker push ghcr.io/mabels/kubectl:$version-$(basename $i)-debian-buster
+  docker buildx build -t $REPO/kubectl:$version-$(basename $i)-ubuntu-jammy --platform $i -f ./Dockerfile.ubuntu ./_output/local/bin/$i 
+  docker push $REPO/kubectl:$version-$(basename $i)-ubuntu-jammy
+  docker buildx build -t $REPO/kubectl:$version-$(basename $i)-debian-buster --platform $i -f ./Dockerfile.debian ./_output/local/bin/$i 
+  docker push $REPO/kubectl:$version-$(basename $i)-debian-buster
 done
 
 for i in ubuntu-jammy debian-buster
 do
-	docker manifest create ghcr.io/mabels/kubectl:$version-$i \
-		--amend ghcr.io/mabels/kubectl:$version-$i-amd64 \
-		--amend ghcr.io/mabels/kubectl:$version-$i-arm64 \
-		--amend ghcr.io/mabels/kubectl:$version-$i-arm
-	docker manifest push ghcr.io/mabels/kubectl:$version-$i
+	docker manifest create $REPO/kubectl:$version-$i \
+		--amend $REPO/kubectl:$version-$i-amd64 \
+		--amend $REPO/kubectl:$version-$i-arm64 \
+		--amend $REPO/kubectl:$version-$i-arm
+	docker manifest push $REPO/kubectl:$version-$i
 
-	docker manifest create ghcr.io/mabels/kubectl:latest-$i \
-		--amend ghcr.io/mabels/kubectl:$version-$i-amd64 \
-		--amend ghcr.io/mabels/kubectl:$version-$i-arm64 \
-		--amend ghcr.io/mabels/kubectl:$version-$i-arm
-	docker manifest push ghcr.io/mabels/kubectl:latest-$i
+	docker manifest create $REPO/kubectl:latest-$i \
+		--amend $REPO/kubectl:$version-$i-amd64 \
+		--amend $REPO/kubectl:$version-$i-arm64 \
+		--amend $REPO/kubectl:$version-$i-arm
+	docker manifest push $REPO/kubectl:latest-$i
 done
 
-docker manifest create ghcr.io/mabels/kubectl:latest \
-	--amend ghcr.io/mabels/kubectl:$version-ubuntu-jammy-amd64 \
-	--amend ghcr.io/mabels/kubectl:$version-ubuntu-jammy-arm64 \
-	--amend ghcr.io/mabels/kubectl:$version-ubuntu-jammy-arm
-docker manifest push ghcr.io/mabels/kubectl:latest
+docker manifest create $REPO/kubectl:latest \
+	--amend $REPO/kubectl:$version-ubuntu-jammy-amd64 \
+	--amend $REPO/kubectl:$version-ubuntu-jammy-arm64 \
+	--amend $REPO/kubectl:$version-ubuntu-jammy-arm
+docker manifest push $REPO/kubectl:latest
 
